2ae1c7f94b6b5296793cc067f6b12f9f19c08371
Chris De Leon <chrisdeleon333@berkeley.edu>
2020-12-20 14:25:11 -0800

    Removing unnecessary import

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
 import layouts
-import joblib
 import model
 import dial
 import dash
